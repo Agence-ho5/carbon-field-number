91ebec8f0cab6e0407e9831ba0c28af7806c3d0d
Fabien LEGE <fabien.lege@ho5group.com>
2020-01-16 00:13:16 +0100

    BUGFIX : not working because initializing before theme initialisation. Converting to mu-plugin

--- field.php
+++ field.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * Plugin Name: Carbon Field Number
+ * Description: Number Field extension for Carbon Fields.
+ * Version: 1.0.0
+ * Author: Fabien LEGE
+ * Author URI: https://agenceho5.com
+ * License: GPL2
+ * Requires at least: 4.0
+ * Tested up to: 4.8
+ * Text Domain: carbon-field-number
+ * Domain Path: /languages
+ */
+
 use Carbon_Fields\Carbon_Fields;
 use Carbon_Field_Number\Number_Field;
 
 define( 'Carbon_Field_Number\\DIR', __DIR__ );
 
-Carbon_Fields::extend( Number_Field::class, function( $container ) {
-	return new Number_Field( $container['arguments']['type'], $container['arguments']['name'], $container['arguments']['label'] );
-} );
+add_action('after_setup_theme', function () {
+	Carbon_Fields::extend( Number_Field::class, function( $container ) {
+		return new Number_Field( $container['arguments']['type'], $container['arguments']['name'], $container['arguments']['label'] );
+	} );
+}, 99);
